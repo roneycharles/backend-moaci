91ff764384c932b632707786fb209d5021b9610f
roneycharles <mmn.charles@@gmail.com>
2020-04-17 01:27:39 -0300

    backend moaci

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        return this.toIntent('HelloWorldIntent');
+    },
+
+    HelloWorldIntent() {
+        this.ask('Bem vindo ao projeto Moaci, qual o seu nome?');
+    },
+
+    MyNameIsIntent() {
+        this.tell('Olá ' + this.$inputs.name.value + ', prazer em conhecê-lo!');
+    },
+});
+
+module.exports.app = app;
